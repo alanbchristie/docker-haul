ca550bfcea0e100c21cb1fd32eca617c43ec7f0f
Alan Christie <alan.christie@matildapeak.com>
2020-10-01 14:52:07 +0100

    - Initial solution

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# docker-haul
-Pull all your container images from docker
+# Docker Haul
+
+![GitHub](https://img.shields.io/github/license/alanbchristie/docker-haul)
+![GitHub tag (latest SemVer)](https://img.shields.io/github/v/tag/alanbchristie/docker-haul)
+
+A simple script that pulls all the container images from docker
+for a user and namespace. Thanks to [Jerry Baker] and his
+`dockerhub-v2-api-user.sh` [gist].
+
+To run the script you will need: -
+
+-   [jq]
+-   docker
+-   Credentials
+
+Set your Docker credentials and the *namespace* of the container images
+that you want to pull, remembering that the namespace does not have to be
+the same as your username...
+
+    $ export DOCKER_USERNAME=alanbchristie 
+    $ export DOCKER_PASSWORD=*******
+    $ export DOCKER_NAMESPACE=alanbchristie 
+
+The pull (haul) the images...
+
+    $ ./haul.sh
+
+The script wil force a pull by first removing each image.
+
+>   Remember that you're pulling everything - so this may take some time,
+    depending on what you're pulling.
+
+If you don't want to keep the pulled images add `--no-keep` to the
+command: -
+
+    $ ./haul.sh --no-keep
+
+---
+
+[jerry baker]: https://gist.github.com/kizbitz
+[gist]: https://gist.github.com/kizbitz/e59f95f7557b4bbb8bf2
+[jq]: https://stedolan.github.io/jq
---
+++ haul.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Haul (pull) images using the Docker Hub V2 API.
+# With thanks to Jerry Baker
+# (https://gist.github.com/kizbitz/e59f95f7557b4bbb8bf2)
+#
+# Pulls all images associated with a Docker Hub user account and namespace
+# (remembering that the namespace and user may not be the same).
+#
+# If you do not want to keep the images add the argument NO-KEEP.
+
+set -e
+
+# Expect username, password and namespace
+: "${DOCKER_USERNAME?Need to set DOCKER_USERNAME}"
+: "${DOCKER_PASSWORD?Need to set DOCKER_PASSWORD}"
+: "${DOCKER_NAMESPACE?Need to set DOCKER_NAMESPACE}"
+
+# Has the user done '--no-keep'?
+NO_KEEP=0
+if [[ $# -gt 0 ]]
+then
+  if [[ ${1} == '--no-keep' ]]
+  then
+    NO_KEEP=1
+  fi
+fi
+
+echo "+> $(date)"
+
+echo "+> Getting API token..."
+TOKEN=$(curl -s -H "Content-Type: application/json" \
+        -X POST \
+        -d '{"username": "'${DOCKER_USERNAME}'", "password": "'${DOCKER_PASSWORD}'"}' \
+        https://hub.docker.com/v2/users/login/ | \
+        jq -r .token)
+
+echo "+> Getting registries..."
+REG_LIST=$(curl -s -H "Authorization: JWT ${TOKEN}" \
+        https://hub.docker.com/v2/repositories/${DOCKER_NAMESPACE}/?page_size=100 | \
+        jq -r '.results|.[]|.name')
+NUM_REG=0
+for i in ${REG_LIST}
+do
+  echo "   ${i}"
+  ((NUM_REG=NUM_REG+1))
+done
+echo "+> Got ${NUM_REG} registries"
+
+echo "+> Getting registry images..."
+NUM_IMAGES=0
+for i in ${REG_LIST}
+do
+  echo "   Getting tags for ${i}..."
+  IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" \
+        https://hub.docker.com/v2/repositories/${DOCKER_NAMESPACE}/${i}/tags/?page_size=100 | \
+        jq -r '.results|.[]|.name')
+  # Compile a list of images with tags
+  # and put assemble them into the FULL_IMAGE_LIST variable
+  NUM_TAGS=0
+  for j in ${IMAGE_TAGS}
+  do
+    FULL_IMAGE_LIST="${FULL_IMAGE_LIST} ${DOCKER_NAMESPACE}/${i}:${j}"
+    ((NUM_IMAGES=NUM_IMAGES+1))
+    ((NUM_TAGS=NUM_TAGS+1))
+  done
+  echo "   (${NUM_TAGS})"
+done
+echo "+> Got ${NUM_IMAGES} images"
+
+# Force a pull of an image. We do this by: -
+# - deleting any local copy
+# - pulling the image
+# - deleting the pulled image (if user used --no-keep)
+echo "+> Hauling images..."
+for i in ${FULL_IMAGE_LIST}
+do
+  echo "   ${i}"
+  docker rmi "${i}" > /dev/null 2>&1 || true
+  docker pull "${i}" > /dev/null 2>&1 || true
+  # If the user's used '--no-haul' now remove the hauled image...
+  if [[ ${NO_KEEP} == 1 ]]
+  then
+    docker rmi "${i}" > /dev/null 2>&1 || true
+  fi
+done
+
+echo "+> Congratulations - u-hauled it all!"
+
+echo "+> $(date)"
